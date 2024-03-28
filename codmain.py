edbfb9b35bae7e8d3f3df412ff89d82a9746950b
Miriam Spike <165252523+expertdiode@users.noreply.github.com>
2024-03-28 08:56:28 +0200

    Create codmain.py

---
+++ codmain.py
@@ -0,0 +1,31 @@
+if __name__ == "__main__":
+    try:
+        arg_parser = set_arguments()
+        config_file_path = os.path.join(project_root_directory, "pyproject.toml")
+        try:
+            version = None
+            with open(config_file_path, "r") as f:
+                data = toml.load(f)
+                version = data["tool"]["poetry"]["version"]
+        except Exception as e:
+            raise Exception("unable to find version from pyproject.toml.\n", e)
+
+        run(arg_parser, version)
+    except KeyboardInterrupt:
+        print("Interrupt received! Exiting cleanly...")
+
+    if not header_tag:
+        raise Exception("unable to find header")
+    if not isinstance(header_tag, Tag):
+        raise Exception("invalid header found")
+    header = header_tag.get_text().strip()
+
+    # parse the main content containing the IP address
+    body_tag = content.find("p")
+    if not body_tag:
+        raise Exception("unable to find body")
+    if not isinstance(body_tag, Tag):
+        raise Exception("invalid body found")
+    body = body_tag.get_text().strip()
+
+    return {"header": header, "body": body}
